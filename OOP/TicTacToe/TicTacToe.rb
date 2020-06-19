42761c20d00194bf2fc75cf6185517d39c408f9b
Lambert Francis <lfrancis18@punahou.edu>
2020-06-19 02:04:54 -0400

    Finished tictactoe game

--- OOP/TicTacToe/TicTacToe.rb
+++ OOP/TicTacToe/TicTacToe.rb
@@ -1,9 +1,10 @@
-
+require_relative 'Board'
 class TicTacToe
-	board = Board.new()
 
 
 	def initialize()
+		@past_moves = []
+		@board = Board.new()
 		@num_moves = 0
 		@game_over = false
 
@@ -13,16 +14,25 @@ class TicTacToe
 		@p2 = gets.chomp
 
 		@turn = @p1
-
-		TicTacToe.playGame()
+		playGame()
 	end
 
-	def self.playGame()
-		while num_moves < 9 && game_over == false
+	def playGame()
+		p "This game uses standard matrix indexing! Input your move as X,Y where X is the Xth row and Y is the Yth column."
+		symbol = 'X'
+		while @num_moves < 9 && @game_over == false
 			p "#{@turn} make your move."
-			move = gets.chomp
-			board.move(move)
-			if board.checkWin?(move) # End game if winner
+			@turn == @p1 ? symbol = 'X' : symbol = 'O'
+			move = gets.chomp.split(',').map {|x| x.to_i}
+			while @past_moves.include?(move)
+				p "That spot is taken! Choose another square."
+				move = gets.chomp.split(',').map {|x| x.to_i}
+			end
+			@past_moves.push(move)
+			@board.move(move, symbol)
+			@num_moves += 1
+			@board.display()
+			if @board.checkWin? # End game if winner
 				@game_over = true
 				@winner = @turn
 				p "#{@turn} wins!"
@@ -33,6 +43,10 @@ class TicTacToe
 				@turn = @p1
 			end
 		end
+		p "It's a tie!" if @game_over == false
 	end
 
 end
+
+newGame = TicTacToe.new()
+newGame.playGame()
--- OOP/TicTacToe/board.rb
+++ OOP/TicTacToe/board.rb
@@ -1,20 +1,100 @@
 class Board
+	attr_accessor :latestMove
 	@latestMove = [nil, nil]
 
 
-	def initialize()
-		@rows = [[nil, nil, nil],[nil, nil, nil], [nil, nil, nil]]
+	def initialize(arr = [[nil, nil, nil],[nil, nil, nil], [nil, nil, nil]])
+		@rows = arr
 	end
 
-	def move(i, j, symbol) # Where i and j correspond to the ith row and jth column
-		@rows[i-1][j-1] = symbol
+	def move(move, symbol) # Where i and j correspond to the ith row and jth column
+		@latestMove = move
+		@rows[move[0] - 1][move[1] - 1] = symbol
+
 	end
 
 	def display()
-		p @latestMove[0]
-		p @latestMove[1]
-		p @latestMove[2]
+		p @rows[0]
+		p @rows[1]
+		p @rows[2]
 	end
 
+	def checkWin?()
+		win = false
+		# Checks if latest move is a winning move.
+		return true if verticalWin?()
+		return true if horizontalWin?()
+		return true if diagonalWin1?()
+		return true if diagonalWin2?()
+		return false
+	end
+
+	def verticalWin?()
+		x, y = @latestMove.map {|z| z-1} # Subtract 1 to convert to indexing starting at 0
+		for i in 0..2
+			return false if @rows[(x+i)%3][y].nil? || @rows[x][y] != @rows[(x+i)%3][y]
+		end
+		return true
+	end
+
+	def horizontalWin?() # Transpose the board and check for a vertical win then transpose back.
+		@rows = @rows.transpose()
+		@latestMove[0], @latestMove[1] = @latestMove[1], @latestMove[0]
+		result = self.verticalWin?()
+		@rows = @rows.transpose()
+		@latestMove[0], @latestMove[1] = @latestMove[1], @latestMove[0]
+		return result
+	end
+
+	def diagonalWin1?() 
+		x, y = @latestMove.map {|z| z-1} # Subtract 1 to convert to indexing starting at 0
+		# Not on diagonal -> return false 
+		if @latestMove[0] != @latestMove[1] && (@latestMove[0] - @latestMove[1]).abs != 2 
+			return false
+		else
+			for i in 0..2
+				return false if @rows[(x+i)%3][(y+i)%3].nil? || @rows[(x+i)%3][(y+i)%3] != @rows[x][y]
+			end
+		end
+		return true
+	end
+
+	def diagonalWin2?() 
+		x, y = @latestMove.map {|z| z-1} # Subtract 1 to convert to indexing starting at 0
+
+		# Not on diagonal -> return false 
+		if @latestMove[0] != @latestMove[1] && (@latestMove[0] - @latestMove[1]).abs != 2 
+			return false
+		else
+			for i in 0..2
+				return false if @rows[(x-i)%3][(y+i)%3].nil? || @rows[(x-i)%3][(y+i)%3] != @rows[x][y]
+			end
+		end
+		return true
+	end
 
 end
+
+rows = [['X','X', 'X'], [nil ,'X', nil], ['O','O','X']]
+board = Board.new(rows)
+board.latestMove = [1,1]
+board.display()
+p board.diagonalWin1?()
+p board.diagonalWin2?()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

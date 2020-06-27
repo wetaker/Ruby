27f51dbbb1c8621968967179a2ec55072dc0378a
Lambert Francis <lfrancis18@punahou.edu>
2020-06-26 22:50:18 -0400

    Fibonacci recursion

--- Recursion/fibonacci.rb
+++ Recursion/fibonacci.rb
@@ -1,10 +1,12 @@
-def fibs(n)
-
-end
-
-
 
 def fibs_rec(n)
+	raise Exception.new("Invalid input") if n<1
 	return 1 if n==1 || n==2
-	return fibs_rec(n-1) 
+	return fibs_rec(n-1) + fibs_rec(n-2)
 end
+
+p fibs_rec(1)
+p fibs_rec(2)
+p fibs_rec(3)
+p fibs_rec(4)
+p fibs_rec(5)

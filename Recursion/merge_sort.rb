b8fe19d1f5a1a4b05434a969f2cfd87ebebda78a
Lambert Francis <lfrancis18@punahou.edu>
2020-06-27 00:01:10 -0400

    Testing two different merge methods, left the one that is generally faster that uses built-in ruby functions

--- Recursion/merge_sort.rb
+++ Recursion/merge_sort.rb
@@ -1,5 +1,5 @@
+require 'benchmark'
 def merge_sort(arr)
-
 	# Base case
 	if arr.length() == 1
 		return arr
@@ -20,14 +20,36 @@ def merge_sort(arr)
 	end
 end
 
-def merge(l1, l2)
-		list = (l1 + l2).sort()
+
+def merge(l1, l2) 
+	list = (l1 + l2).sort()
 end
 
+# Alternate merge 
+# def merge(l1, l2)
+# 	list = []
+# 	# Merge lists in sorted order
+# 	while !l1.empty? && !l2.empty?
+# 		if l1[0] < l2[0]
+# 			list.push(l1.slice!(0))
+# 		else
+# 			list.push(l2.slice!(0))
+# 		end
+# 	end
+
+# 	# Empty left-over elements
+# 	while !l1.empty?
+# 		list.push(l1.slice!(0))
+# 	end
+# 	while !l2.empty?
+# 		list.push(l2.slice!(0))
+# 	end
+
+# 	return list
+# end
 
 
-test_arr = Array.new(10) {rand(0..5)}
-p test_arr 
-p merge_sort(test_arr)
 
+test_arr = Array.new(100000) {rand(0..5)}
+p test_arr
 
